fd4c64ec35665e9faa916ca3f0eec49d67fc3d00
eun-choi <54615424+eun-choi@users.noreply.github.com>
2021-11-30 20:39:49 +0900

    Revert "bugfix"
    This reverts commit 4550200b732c3d776c795fb9a71cfc268ef9d817.

--- hooks/dependencies-gradle-hook.js
+++ hooks/dependencies-gradle-hook.js
@@ -14,8 +14,7 @@ module.exports = (context) => {
   const imglyDependencies =
     "\n" +
     BLOCK_START +
-    `
-    classpath "ly.img.android.sdk:plugin:9.1.0"` +
+    `classpath "ly.img.android.sdk:plugin:9.1.0"` +
     "\n" +
     BLOCK_END +
     "\n";
